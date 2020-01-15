135f11e71d8e135898ff55a1e7eeef688d0b203d
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-01-15 16:06:07 -0500

    Made the security rules tighter, also added cloud function to limit pics per user
    I've decided to rather differentiate raw images and scaled images by their storage location, not their names.
    This way, I don't have to worry about the image prefixes in the security rules.
    Additionally, a change in client logic made setUserProfilePicURL obsolete, so it's gone.
    Lastly, I've added logic to ensure that every user only has one profile picture stored in the storage bucket

--- functions/src/cloudStorageFunctions.ts
+++ functions/src/cloudStorageFunctions.ts
@@ -1,19 +1,23 @@
-import admin = require('firebase-admin');
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import { DeleteFileResponse } from '@google-cloud/storage';
 const path = require('path');
 
+// Creating our cloud client
+const bucket = admin.storage().bucket()
 
-//I'll be keeping track of this so that this function only really does something
-//when the image ends in the extension that it gets when it's resized.
-//Sure, this can be easily bypassed, but it will save a lot of computation 
-//for most cases
-const EXPECTED_EXTENSION = "_100x100"
-
-export const setUserProfilePicURL = functions.storage.object().onFinalize(async (object) => {
-    if (!object.name?.endsWith(EXPECTED_EXTENSION)) return;
-    const ownerUid = path.basename(path.dirname(object.name))
-
-    const ownerSnippetSnapshot = await admin.database().ref(`userSnippets/${ownerUid}`).once('value');
-    if (!ownerSnippetSnapshot.exists()) return; //Don't bother deleting the picture, that'll be handled by a cleanup later
-    await admin.database().ref(`userSnippets/${ownerUid}/profilePicPath`).set(object.name)
+/**
+ * This function deletes a user's older profile pic after a new one is uploaded
+ * It is only listeneing for pictures created by the image resizer
+ */
+export const deleteOlderProfilePic = functions.storage.object().onFinalize(async (object) => {
+    const picFolder = path.basename(path.dirname(object.name))
+    if (picFolder !== "scaled") return;
+    const allProfilepics = await bucket.getFiles({directory: path.dirname(object.name)})
+    const deletionPromises: Array<Promise<DeleteFileResponse>> = []
+    allProfilepics[0].forEach((file)  => {
+        if (file.name !== object.name) 
+            deletionPromises.push(bucket.file(file.name).delete())
+    });
+    await Promise.all(deletionPromises)
 });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp({
+    ...functions.config().firebase,
+    storageBucket: "the-og-lunchme.appspot.com"
+});
 
 export * from './friendRequestFunctions'
 export * from './activeBroadcastFunctions'
