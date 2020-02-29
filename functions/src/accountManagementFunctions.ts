d796e0d71282df2864231aee0f40dcc008103c28
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-02-29 17:37:51 -0500

    Added cloud functions for proerly making and editing user snippets

---
+++ functions/src/accountManagementFunctions.ts
@@ -0,0 +1,88 @@
+import * as functions from 'firebase-functions';
+import * as standardHttpsData from './standardHttpsData'
+import admin = require('firebase-admin');
+
+const database = admin.database()
+
+interface snippetCreationRequest{
+    displayName:string,
+    username:string
+}
+
+export const createSnippet = functions.https.onCall(
+    async (data : snippetCreationRequest, context) => {
+    
+    if (!context.auth) {
+        throw standardHttpsData.notSignedInError()
+    }
+  
+    if (typeof data.displayName !== "string" || typeof data.username !== "string"){
+        throw new functions.https.HttpsError("invalid-argument", 
+        "Both arguments should be strings")
+    }
+
+    //The Username string can only have a-z, 0-9 or _ or -
+    //Display names can be whatever the user wants them to be
+    const regexTest = RegExp(/^[a-z0-9_-]+$/)
+
+    const normalizedDisplayName = data.displayName.normalize("NFKC")
+    const normalizedUsername = data.username.normalize('NFKC') 
+    const lowerNormalisedUsername = normalizedUsername.toLowerCase()
+    const lowerNormalisedDisplayName= normalizedDisplayName.toLowerCase()
+
+    if (!regexTest.test(lowerNormalisedUsername)){
+        throw new functions.https.HttpsError("invalid-argument", 
+        "Only A-Z, a-z, 0-9 or _ or - allowed for username")
+    }
+
+    //Making sure the snippet doesn't already exist
+    const snippetRef = database.ref(`/userSnippets/${context.auth.uid}`);
+
+    if ((await snippetRef.once('value')).exists()){
+        throw new functions.https.HttpsError('failed-precondition', "Snippet already exists")
+    }
+
+    //Now checking if the username is in use already
+    const usernameRef = database.ref(`/usernames/${lowerNormalisedUsername}`)
+
+    await usernameRef.transaction(function(currentOwnerUid) {
+        if (currentOwnerUid === null || currentOwnerUid === context.auth?.uid) {
+            //We're also checking if she's the previous owner incase this
+            //function ever fails after this transaction (becase the 
+            //snippet might not be created but the username would)
+            return context.auth?.uid;
+        } else {
+            throw new functions.https.HttpsError('failed-precondition',
+                "Username already in use!")
+        }
+    });
+
+    await snippetRef.set({
+        username: normalizedUsername,
+        usernameQuery: lowerNormalisedUsername,
+        displayName: normalizedDisplayName,
+        displayNameQuery: lowerNormalisedDisplayName
+    }) 
+});
+
+export const updateDisplayName = functions.https.onCall(
+    async (data : string, context) => {
+    
+    if (!context.auth) {
+        throw standardHttpsData.notSignedInError()
+    }
+  
+    if (typeof data !== "string"){
+        throw new functions.https.HttpsError("invalid-argument", 
+        "Arguments should be a string")
+    }
+
+    const normalizedDisplayName = data.normalize("NFKC")
+    const lowerNormalisedDisplayName = normalizedDisplayName.toLowerCase()
+
+    const updates = {} as any
+    updates[`/userSnippets/${context.auth?.uid}/displayName`] = normalizedDisplayName
+    updates[`/userSnippets/${context.auth?.uid}/displayNameQuery`] = lowerNormalisedDisplayName
+    await database.ref().update(updates);
+    return {status: standardHttpsData.returnStatuses.OK}
+});
