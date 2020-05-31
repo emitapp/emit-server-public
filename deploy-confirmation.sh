06e28ec643365833c514790723a9b0ccc013f796
Anderson <34724755+andersonaddo@users.noreply.github.com>
2020-05-31 01:44:49 -0400

    Added predeploy confirmation script

---
+++ deploy-confirmation.sh
@@ -0,0 +1,17 @@
+echo "🔥🔥🔥"
+echo "You are about to deploy to: "
+projectname=$(firebase use)
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+YELLOW='\033[1;33m' 
+NC='\033[0m' # No Color
+echo "${YELLOW}${BOLD}${projectname}${NC}${NORMAL}"
+echo "🔥🔥🔥"
+
+read -p "Do you wish to continue? " -n 1 -r
+echo    # move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+# handle exits from shell or function but don't exit interactive shell
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 
+fi
